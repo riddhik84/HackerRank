19f128226c9dbcb44b1ad0dfd35f9ddfa9e497d9
riddhik84 <riddhik84@gmail.com>
2015-05-25 13:24:28 +0530

    HackerRank

---
+++ HackerRank/src/com/java/challenges/EndOfFile.java
@@ -0,0 +1,20 @@
+package com.java.challenges;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		String fileLine;
+		int lineCounter = 1;
+
+		while (in.hasNextLine())
+		{
+			fileLine = in.nextLine();
+			System.out.println(lineCounter + " " + fileLine);
+			lineCounter++;
+		}
+	}
+}
