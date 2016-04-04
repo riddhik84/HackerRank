03f7bad876b48bae9e036f10404813f4198e4463
riddhik84 <riddhik84@gmail.com>
2016-04-04 15:01:29 +0530

    Algorithm solutions

---
+++ HackerRank/src/com/hackerrank/challenges/algorithm/Staircase.java
@@ -0,0 +1,26 @@
+package com.hackerrank.challenges.algorithm;
+
+import java.util.*;
+
+public class Staircase {
+	
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		int blanks = N - 1;
+		int prints = N - blanks;
+		
+		for(int i = 0; i < N; i++){
+			for(int j =0; j < blanks; j++){
+				System.out.print(" ");
+			}
+			blanks--;
+			for(int k = 0; k < prints; k++){
+				System.out.print("#");
+			}
+			prints++;
+			System.out.print("\n");
+		}
+	}
+}
