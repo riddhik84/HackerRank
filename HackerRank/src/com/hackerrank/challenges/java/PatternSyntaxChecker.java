3aa049e4a5ae23acd00a995f9eb37be22c6daa0c
Riddhi K <riddhik84@gmail.com>
2017-07-06 18:54:50 +0530

    Solution

---
+++ HackerRank/src/com/hackerrank/challenges/java/PatternSyntaxChecker.java
@@ -0,0 +1,24 @@
+package com.hackerrank.challenges.java;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntaxChecker {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+		while(testCases > 0){
+			String pattern = in.nextLine();
+			try{
+				Pattern pat = Pattern.compile(pattern);
+				System.out.println("Valid");
+
+			} catch(PatternSyntaxException pse){
+				System.out.println("Invalid");
+			}
+			testCases--;
+		}
+	}
+}
